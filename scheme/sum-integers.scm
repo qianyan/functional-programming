419d615794557bd2277b6bf211e9f2a954f561ac
Yan Qian <qianyan@thoughtworks.com>
2014-11-24 23:48:58 +0800

    more abstract for scheme sum from a to b

--- scheme/sum-integers.scm
+++ scheme/sum-integers.scm
@@ -2,3 +2,17 @@
   (if  (> a b) 
     0
     (+ a  (sum-integers  (+ a 1) b))))
+
+
+(define (sum term a next b)
+  (if (> a b)
+    0
+    (+ (term a) 
+       (sum term (next a) next b))))
+
+(define (inc n) (+ n 1))
+
+(define (cube n) (* n n n))
+
+(define (sum-cubes a b) 
+  (sum cube a inc b))
