b87c27d0b8b21b534db9e57d5208863db6b69c12
Yan Qian <qianyan@thoughtworks.com>
2015-01-14 00:12:03 +0800

    add reduce with extend test

--- javascript/test/ObjectOps.unit.js
+++ javascript/test/ObjectOps.unit.js
@@ -11,5 +11,28 @@ module.exports = testCase({
         });
         test.deepEqual(actualObj, expectedObj);
         test.done();
-    }
+    },
+
+    "should get a values array for an object": function (test) {
+        var obj = {"001": "YES", "002": "NO", "003": "W"};
+        var expectedArray = ['YES', 'NO', 'W'];
+        var actualArray = _.map(obj, _.identity);
+
+        test.deepEqual(actualArray, expectedArray);
+        test.done();
+    },
+
+     "should reduce an array to an object": function (test) {
+        var array = ['YES', 'NO', 'W'];
+
+        var actual = _.reduce(_.map(array, function(v, i) {
+            var o = {};
+            o[ "00" + (i+1) ] = v;
+            return o;
+        }) , function(memo, next) { return _.extend(memo, next); }, {});
+        var expected = {"001": "YES", "002": "NO", "003": "W"};
+
+        test.deepEqual(actual, expected);
+        test.done();
+    }   
 })
