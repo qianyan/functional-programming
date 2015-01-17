ea5f2f781d450a4b817a9efb791084ea309c2d07
Yan Qian <qianyan@thoughtworks.com>
2015-01-17 23:51:37 +0800

    add an object with private field

--- javascript/src/Closure.js
+++ javascript/src/Closure.js
@@ -5,5 +5,13 @@ module.exports = {
         return function(obj) {
             return obj && obj[field];
         }
-    }
+    },
+    O: (function (){
+        var PRIVATE = 0;
+        return {
+            inc: function(num) {
+                return PRIVATE += num;
+            }
+        };
+    })()
 };
--- javascript/test/Closure.unit.js
+++ javascript/test/Closure.unit.js
@@ -6,5 +6,11 @@ module.exports = testCase({
         var obj = {name: "ryan"};
         test.equal(C.pluck('name')(obj), 'ryan');
         test.done();
+    },
+    "should get object with a private field": function(test) {
+        test.equal(C.O.inc(1), 1);
+        test.equal(C.O.inc(2), 3);
+        test.equal(C.O.inc(1), 4);
+        test.done();
     }
 });
